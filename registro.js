0f9c97dc6cb1d04313699b9f7e490dce9f7a5abd
joaquin <joaquin.ma41168@cesurformacion.com>
2021-11-03 13:19:22 +0100

    Validaciones de registro ok

--- registro.js
+++ registro.js
@@ -9,11 +9,36 @@ function validaNombre(usuario){
 var valor=document.getElementById("emailsignup").value;
 
 function pruebaemail(valor){
-    re=/^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
-    if(!re.exec(valor)){
-        alert('email no valido');
-    }
+  let expresion=/^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+  let correo=document.getElementById('emailsignup');
+  if(!expresion.exec(valor)){
+      alert('email no válido, rellenelo de nuevo');
+     
+      correo.value = ''; // Si no es correcto vaciamos la celda
+      correo.style.backgroundColor="red"; // Y se colorea en rojo indicándolo
+
+   
+  }
 }
 
+function Fcontraseña(){
+  var p1 = document.getElementById("contrasena1").value;
+  var p2 = document.getElementById("contrasena2").value;
+      
+    //no campo vacio
+    if (p1.length == 0 || p2.length == 0) {
+        alert("Los campos de la contraseña no pueden quedar vacios");
+        return false;
+      }
 
+    //ambas contraseñas coinciden
+    if (p1 != p2) {
+        alert("Las contraseñas deben de coincidir");
+        return false;
+      } else {
+        alert("Contraseñas correctas");
+        return true; 
+      }
+      
+ }
 
